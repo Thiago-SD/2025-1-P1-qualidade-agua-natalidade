6f19306c5452b204db728ef8680dfde0683d5c64
AwsumName03 <th.damasceno@hotmail.com>
2025-07-15 01:09:06 -0300

    Feita agregação dos dataframes pandas

--- projeto1.py
+++ projeto1.py
@@ -1,20 +1,27 @@
 import requests
 import pandas as pd
 
-# URL da API para o Sistema de Informação sobre Nascidos Vivos
 url_sinasc = "https://apidadosabertos.saude.gov.br/vigilancia-e-meio-ambiente/sistema-de-informacao-sobre-nascidos-vivos"
 url_sisagua = "https://apidadosabertos.saude.gov.br/sisagua/controle-semestral"
 
-# Parâmetros da requisição (ajuste conforme necessário)
 params = {
-    "limit": 100,  # Número máximo de registros por página
-    "offset": 0     # Deslocamento para paginação
+    "limit": 100,  
+    "offset": 0     
 }
 
-response_sinasc = requests.get(url_sinasc, params=params)
-response_sisagua = requests.get(url_sisagua, params=params)
- 
-df_sinasc = pd.DataFrame(response_sinasc.json())
-df_sisagua = pd.DataFrame(response_sisagua.json())
+def main():
+    response_sinasc = requests.get(url_sinasc, params=params)
+    response_sisagua = requests.get(url_sisagua, params=params)
+    
+    df_sinasc = pd.DataFrame(response_sinasc.json()['sinasc'])
+    df_sisagua = pd.DataFrame(response_sisagua.json()['parametros'])
 
-print(df_sisagua.describe())
+    print(f"Colunas importadas do dataset Sinasc:\n{df_sinasc.head()}")
+    print(f"Colunas importadas do dataset Sisagua:\n{df_sisagua.head()}")
+
+    merged_df = pd.merge(df_sinasc, df_sisagua, left_on='codmunnatu', right_on='codigo_ibge', how='left')
+
+    print(f"Dados agregados de ambos os datasets:\n{merged_df.head()}")
+
+if __name__ == "__main__":
+    main()
